08abda37ec57208c388c3902bb818680aa327b66
nothing628 <yogyphang@gmail.com>
2022-05-22 22:14:03 +0700

    fix thumbnail video

--- resources/views/components/video-thumb.blade.php
+++ resources/views/components/video-thumb.blade.php
@@ -1,5 +1,7 @@
 <div class="w-72 px-2 mb-4">
-    <img class="mb-2" src="{{ asset('test.png')}}" />
-    <p class="mb-1 text-base">Test Video</p>
+    <div class="w-full h-[153px] relative bg-gray-700 mb-2 bg-contain bg-no-repeat bg-center" style="background-image: url('{{asset('test2.jpg')}}')">
+        <div class="absolute bottom-0 right-0 bg-black text-white text-xs px-1 py-1">05:00</div>
+    </div>
+    <p class="mb-1 text-sm">Test Video</p>
     <p class="text-xs">7 Hours ago</p>
 </div>
