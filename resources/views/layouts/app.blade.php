50329b4783487d1a8fd1efa2c944f536f764b998
nothing628 <yogyphang@gmail.com>
2022-05-22 21:39:25 +0700

    add app layout

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/all.min.css') }}" rel="stylesheet" />
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
+</head>
+
+<body>
+    <div class="" id="app">
+        <x-header></x-header>
+        <div class="block relative min-h-screen pt-16">
+            <x-sidebar></x-sidebar>
+            <x-content>
+                @yield('content')
+            </x-content>
+        </div>
+    </div>
+
+    <script src="{{ mix('js/app.js') }}"></script>
+</body>
+
+</html>
