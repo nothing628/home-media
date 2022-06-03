fc90c81c58d80c47bf9b7cceee56feb4a0a1c80a
nothing628 <yogyphang@gmail.com>
2022-06-03 22:20:10 +0700

    add more detail video info at video player

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,7 +1,10 @@
 <x-guest-layout>
     <div>
-        <video width="640" controls>
+        <video width="640" controls class="w-full mb-4">
             <source src="{{$url}}" type="video/mp4"></source>
         </video>
+
+        <h1 class="text-2xl font-bold">{{$video->title}}</h1>
+        <p class="text-sm text-gray-700">{{$video->updated_at->diffForHumans()}}</p>
     </div>
 </x-guest-layout>
