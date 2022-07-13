ecec8d2bbaec0de5b5654f609073c02de8ccc98d
nothing628 <yogyphang@gmail.com>
2022-07-14 00:03:12 +0700

    fix slider animation

--- resources/js/components/slider/container.vue
+++ resources/js/components/slider/container.vue
@@ -1,19 +1,65 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed, useSlots, onMounted, onBeforeUnmount } from 'vue'
 import SliderSelector from './selector.vue'
 
-const totalPages = ref(3)
+const container = ref()
+const containerWidth = ref(0)
+const totalPages = ref(1)
 const currentPage = ref(1)
 const handleSelect = (index: number) => {
     currentPage.value = index
 }
+
+const totalWidth = computed(() => containerWidth.value * totalPages.value)
+const totalTranslateX = computed(() => containerWidth.value * (currentPage.value - 1) * -1)
+
+const slots = useSlots()
+
+const timerId = ref(null)
+const moveNextPage = () => {
+    let nextPage = currentPage.value + 1
+    if (nextPage > totalPages.value) nextPage = 1
+
+    currentPage.value = nextPage
+}
+const startTimer = () => {
+    let id = setInterval(moveNextPage, 2000)
+
+    timerId.value = id
+}
+const stopTimer = () => {
+    if (timerId.value) {
+        clearTimeout(timerId.value)
+        timerId.value = null
+    }
+}
+
+onBeforeUnmount(stopTimer)
+onMounted(() => {
+    const defaultSlot = slots.default
+
+    if (container.value) {
+        containerWidth.value = container.value.clientWidth
+    }
+
+    if (typeof defaultSlot == 'function') {
+        const childs = defaultSlot()
+
+        totalPages.value = childs.length
+        currentPage.value = 1
+    }
+
+    startTimer()
+})
 </script>
 
 <template>
     <div class="block w-full z-10 relative">
-        <div class="relative overflow-hidden">
-            <div class="relative"
-                style="transform: translate3d(0px, 0px, 0px); transition: all 1s ease 0s; width: 3255px;">
+        <div class="relative overflow-hidden" ref="container">
+            <div class="relative" style="transition: all 1s ease 0s;" :style="{
+                width: totalWidth + 'px',
+                transform: `translate3d(${totalTranslateX}px, 0px, 0px)`
+            }">
                 <slot></slot>
             </div>
         </div>
