ac8d57e3aff26559913226514c7fa179766b756e
nothing628 <yogyphang@gmail.com>
2022-07-14 14:16:17 +0700

    add footer

--- resources/views/components/sidebar/menu-item.blade.php
+++ resources/views/components/sidebar/menu-item.blade.php
@@ -1,15 +1,15 @@
-<li class="list-item">
+<li class="list-item relative">
     <a class="block text-white text-sm text-center font-semibold py-2 px-[14px] w-[90px] md:w-[225px] md:text-left xl:px-3 xl:py-[14px] {{ isset($active) ? 'bg-white/20' : '' }}"
         href="{{ $href }}">
         {{ $slot }}
         <span class="block text-[11px] m-0 md:inline md:text-[13px] md:ml-[11px]">{{ $title }}</span>
         @if (isset($dropdown))
-        <i class="fas fa-fw fa-angle-down float-right text-center block w-4"></i>
+        <i class="fas fa-fw fa-angle-down float-right text-center hidden md:block w-4"></i>
         @endif
     </a>
 
     @if (isset($dropdown))
-        <div class="static top-0 block float-none mx-4 py-2 bg-white rounded-sm">
+        <div class="absolute md:static left-[90px] top-0 block float-none mx-0 md:mx-4 py-2 bg-white border border-black/10 rounded-sm min-w-[10rem]">
             {{ $dropdown }}
         </div>
     @endif
