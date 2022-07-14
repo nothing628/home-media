b1768fd7512c215d581edb4750a850a785605bea
nothing628 <yogyphang@gmail.com>
2022-07-14 19:02:25 +0700

    change upload page style

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -1,25 +1,7 @@
 @extends('layouts.guest')
 
 @section('content')
-    <form method="post" enctype="multipart/form-data">
-        <h1 class="text-3xl mb-4">Upload New Video</h1>
-
-        @csrf
-        <div class="w-full mb-4">
-            <label class="font-bold text-sm mb-2 inline-block w-full" for="title">Title :</label>
-            <input class="border border-gray-400 text-slate-700 h-8 w-full px-2 rounded-md text-base outline-0" type="text"
-                name="title" id="title" />
-        </div>
-
-        <div class="w-full mb-4">
-            <label class="font-bold text-sm mb-2 inline-block w-full" for="file">Video :</label>
-            <input accept="video/mp4" type="file" name="file" id="file" />
-        </div>
-        <div class="w-full mb-4">
-            <button class="bg-green-600 text-white px-4 py-3 border border-green-600 rounded-md">Upload <i
-                    class="fas fa-cloud-upload"></i></button>
-        </div>
-    </form>
+    <upload-page></upload-page>
 @endsection
 
 @section('footer')
