daebc1a457801cf43f9d250e1f902600c59ec5e7
nothing628 <yogyphang@gmail.com>
2022-07-17 21:52:21 +0700

    add upload success page

--- resources/js/components/upload/upload-home.vue
+++ resources/js/components/upload/upload-home.vue
@@ -13,7 +13,7 @@ const upload = () => {
         <div class="mx-auto text-center pt-12 pb-12 flex-shrink-0 flex-grow-0 w-full md:w-2/3">
             <h1 class="mb-2"><i class="fas fa-file-upload text-[200px] text-[#07bf67]"></i></h1>
             <h4 class="mt-12 mb-2 text-[#333] text-2xl font-medium">Select Video files to upload</h4>
-            <p class="">or drag and drop video files</p>
+            <p>or drag and drop video files</p>
             <div class="mt-6">
                 <button @click="upload"
                     class="text-center text-[#07bf67] text-base leading-6 border border-[#07bf67] py-1.5 px-3">Upload
--- resources/js/components/upload/upload-success.vue
+++ resources/js/components/upload/upload-success.vue
@@ -1,3 +1,9 @@
 <template>
-    <div>Upload Success</div>
+    <div class="flex flex-wrap -mx-[15px]">
+        <div class="mx-auto text-center pt-12 pb-12 flex-shrink-0 flex-grow-0 w-full md:w-2/3">
+            <h1 class="mb-2"><i class="fas fa-box-check text-[200px] text-[#07bf67]"></i></h1>
+            <h4 class="mt-12 mb-2 text-[#333] text-2xl font-medium">Upload Success</h4>
+            <p>Please wait until we finish process your video metadata</p>
+        </div>
+    </div>
 </template>
