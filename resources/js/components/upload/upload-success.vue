e52020020e754a666d9b1ec38cb8b78671e73bd2
nothing628 <yogyphang@gmail.com>
2022-07-17 21:56:19 +0700

    add button to back home

--- resources/js/components/upload/upload-success.vue
+++ resources/js/components/upload/upload-success.vue
@@ -4,6 +4,12 @@
             <h1 class="mb-2"><i class="fas fa-box-check text-[200px] text-[#07bf67]"></i></h1>
             <h4 class="mt-12 mb-2 text-[#333] text-2xl font-medium">Upload Success</h4>
             <p>Please wait until we finish process your video metadata</p>
+
+            <div class="mt-6">
+                <a href="/"
+                    class="text-center text-[#07bf67] text-base leading-6 border border-[#07bf67] py-1.5 px-3">Back to
+                    Home</a>
+            </div>
         </div>
     </div>
 </template>
