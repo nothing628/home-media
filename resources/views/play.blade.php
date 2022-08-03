71e8cf9420da47deffc9805703a80a81c29d6bcc
nothing628 <yogyphang@gmail.com>
2022-08-03 22:36:03 +0700

    fix play page footer

--- resources/views/play.blade.php
+++ resources/views/play.blade.php
@@ -1,4 +1,6 @@
-<x-guest-layout>
+@extends('layouts.guest')
+
+@section('content')
     <div class="max-w-6xl mx-auto">
         <video width="640" controls class="w-full mb-4">
             <source src="{{ $url }}" type="video/mp4">
@@ -7,8 +9,13 @@
 
         <video-title title="{{ $video->title }}" :views="20000"></video-title>
         <video-channel channel-name="Administrator" channel-avatar="{{ asset('images/login.png') }}"
-            :channel-subscriber-count="1000000000" is-channel-verified published-at="{{ $video->updated_at->toISOString() }}">
+            :channel-subscriber-count="1000000000" is-channel-verified
+            published-at="{{ $video->updated_at->toISOString() }}">
         </video-channel>
         <video-description description="Lorem ipsum" :tags="['test']"></video-description>
     </div>
-</x-guest-layout>
+@endsection
+
+@section('footer')
+    <x-footer.mini-footer></x-footer.mini-footer>
+@endsection
