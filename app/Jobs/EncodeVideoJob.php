13167060b4d6bdf39fb2656872350b7ea818e7ed
nothing628 <yogyphang@gmail.com>
2022-11-05 23:45:50 +0700

    add playlist field and encode video job

--- app/Jobs/EncodeVideoJob.php
+++ app/Jobs/EncodeVideoJob.php
@@ -8,11 +8,16 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use FFMpeg\Format\Video\X264;
+use FFMpeg\FFMpeg as FFMpegBin;
+use ProtoneMedia\LaravelFFMpeg\Support\FFMpeg;
 use App\Models\Video;
 
 class EncodeVideoJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $timeout = 7200;
 
     public $video;
 
@@ -34,5 +39,43 @@ class EncodeVideoJob implements ShouldQueue
     public function handle()
     {
         $video = $this->video;
+        $videoPublicPath = $video->public_path;
+
+        $this->encodeVideo($videoPublicPath);
+    }
+
+    public function encodeVideo($videoPath)
+    {
+        $lowBitrate = (new X264)->setKiloBitrate(1000);
+        $midBitrate = (new X264)->setKiloBitrate(3000);
+        $highBitrate = (new X264)->setKiloBitrate(5000);
+
+        $processOutput = FFMpeg::fromDisk('local')
+            ->open($videoPath)
+            ->exportForHLS()
+            ->setSegmentLength(10)
+            ->setKeyFrameInterval(48)
+            ->addFormat($lowBitrate, function ($media) {
+                $media->addFilter('scale=w=640:h=480');
+            })
+            ->addFormat($midBitrate, function ($media) {
+                $media->addFilter('scale=w=1280:h=720');
+            })
+            ->addFormat($highBitrate, function ($media) {
+                $media->addFilter('scale=w=1920:h=1080');
+            })
+            // ->useSegmentFilenameGenerator(function ($name,$format,$key, callable $segments, callable $playlists ) {
+            //     $segments("{$name}-{$format->getKiloBitrate()}-{$key}-%03d.ts");
+            //     $playlists("{$name}-{$format->getKiloBitrate()}-{$key}.m3u8");
+            // })
+            ->save('test.m3u8');
+
+        $processOutput->all();
+    }
+
+    public function storeMasterPlaylist($masterPlaylistPath, Video $video)
+    {
+        $video->master_playlist_path = $masterPlaylistPath;
+        $video->save();
     }
 }
