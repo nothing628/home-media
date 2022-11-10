2ba7ddb3ce1ec84dc724e429ada90aad76516805
nothing628 <yogyphang@gmail.com>
2022-11-10 07:19:37 +0700

    remove dashboard blade template

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
     public function showDashboardView()
     {
-        return view('dashboard');
+        return Inertia::render('Dashboard');
     }
 }
